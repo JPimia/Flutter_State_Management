dc4e729b34668c8e1879b8eed879a091e06a1362
JPimia <90615229+JPimia@users.noreply.github.com>
2024-11-05 12:57:48 +0200

    riverpod tunings

--- riverpod/lib/providers/news_provider.dart
+++ riverpod/lib/providers/news_provider.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import '/models/news_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NewsModel with ChangeNotifier {
-  List<News> _newsArticles = [
+final newsProvider =
+    StateNotifierProvider<NewsModel, List<News>>((ref) => NewsModel());
+
+class NewsModel extends StateNotifier<List<News>> {
+  NewsModel() : super([]);
+  List<News> newsArticles = [
     News(title: "News header 1"),
     News(title: "News header 2"),
     News(title: "News header 3"),
   ];
 
-  List<News> get news => _newsArticles;
+  List<News> get news => state;
 
   void editNews(int index, String newContent) {
-    news[index].title = newContent;
-    notifyListeners();
+    state = [
+      for (int i = 0; i < state.length; i++)
+        if (i == index) News(title: newContent) else state[i]
+    ];
   }
 }
--- riverpod/lib/screens/news_screen.dart
+++ riverpod/lib/screens/news_screen.dart
@@ -1,68 +1,63 @@
 import 'package:flutter/material.dart';
 import '/providers/news_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class NewsScreen extends StatelessWidget {
+class NewsScreen extends ConsumerWidget {
   const NewsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer<NewsModel>(
-      builder: (context, newsProvider, index) {
-        if (newsProvider.news.isEmpty) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          return ListView.builder(
-            itemCount: newsProvider.news.length,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final news = ref.watch(newsProvider);
+    return news.isEmpty
+        ? const Center(child: CircularProgressIndicator())
+        : ListView.builder(
+            itemCount: news.length,
             itemBuilder: (context, index) {
               return ListTile(
-                title: Text(newsProvider.news[index].title),
+                title: Text(news[index].title),
                 trailing: ElevatedButton(
                   onPressed: () {
                     // You can add more logic for "View" action here
-                    _showEditDialog(
-                        context, newsProvider.news[index].title, index);
+                    _showEditDialog(context, news[index].title, index);
                   },
                   child: Text("View"),
                 ),
               );
             },
           );
-        }
-      },
-    );
   }
+}
 
-  void _showEditDialog(BuildContext context, String newsArticle, int index) {
-    final TextEditingController controller =
-        TextEditingController(text: newsArticle);
+void _showEditDialog(BuildContext context, String newsArticle, int index) {
+  final TextEditingController controller =
+      TextEditingController(text: newsArticle);
 
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text("News Article"),
-          content: TextField(
-            controller: controller,
-            decoration: InputDecoration(hintText: "Enter news article"),
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("News Article"),
+        content: TextField(
+          controller: controller,
+          decoration: InputDecoration(hintText: "Enter news article"),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("Close"),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("Close"),
-            ),
-            TextButton(
-              onPressed: () {
-                context.read<NewsModel>().editNews(index, controller.text);
-                Navigator.of(context).pop();
-              },
-              child: Text("Save"),
-            ),
-          ],
-        );
-      },
-    );
-  }
+          TextButton(
+            onPressed: () {
+              context.read<NewsModel>().editNews(index, controller.text);
+              Navigator.of(context).pop();
+            },
+            child: Text("Save"),
+          ),
+        ],
+      );
+    },
+  );
 }
