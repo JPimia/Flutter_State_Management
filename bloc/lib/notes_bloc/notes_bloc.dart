14da42dfe715717a2b348dde96f6abb66e054253
AmabitPimia <jari.pimia@amabit.fi>
2024-11-14 14:58:01 +0200

    refactor notes_bloc

--- bloc/lib/notes_bloc/notes_bloc.dart
+++ bloc/lib/notes_bloc/notes_bloc.dart
@@ -7,29 +7,33 @@ class NotesBloc extends Bloc<NotesEvent, NotesState> {
   List<String> _notes = [];
 
   NotesBloc() : super(NotesLoading()) {
-    on<AddNote>((event, emit) {
-      final updatedNotes = List<String>.from(_notes);
-      updatedNotes.add(event.note);
+    on<AddNote>(_onAddNote);
+    on<EditNote>(_onEditNote);
+    on<DeleteNote>(_onDeleteNote);
+  }
+
+  Future<void> _onAddNote(AddNote event, Emitter<NotesState> emit) async {
+    final updatedNotes = List<String>.from(_notes);
+    updatedNotes.add(event.note);
+    _notes = updatedNotes;
+    emit(NotesLoaded(notes: updatedNotes));
+  }
+
+  Future<void> _onEditNote(EditNote event, Emitter<NotesState> emit) async {
+    if (state is NotesLoaded) {
+      final updatedNotes = List<String>.from((state as NotesLoaded).notes);
+      updatedNotes[event.index] = event.newNote;
       _notes = updatedNotes;
       emit(NotesLoaded(notes: updatedNotes));
-    });
+    }
+  }
 
-    on<EditNote>((event, emit) {
-      if (state is NotesLoaded) {
-        final updatedNotes = List<String>.from((state as NotesLoaded).notes);
-        updatedNotes[event.index] = event.newNote;
-        _notes = updatedNotes;
-        emit(NotesLoaded(notes: updatedNotes));
-      }
-    });
-
-    on<DeleteNote>((event, emit) {
-      if (state is NotesLoaded) {
-        final updatedNotes = List<String>.from(_notes);
-        updatedNotes.removeAt(event.index);
-        _notes = updatedNotes;
-        emit(NotesLoaded(notes: updatedNotes));
-      }
-    });
+  Future<void> _onDeleteNote(DeleteNote event, Emitter<NotesState> emit) async {
+    if (state is NotesLoaded) {
+      final updatedNotes = List<String>.from(_notes);
+      updatedNotes.removeAt(event.index);
+      _notes = updatedNotes;
+      emit(NotesLoaded(notes: updatedNotes));
+    }
   }
 }
